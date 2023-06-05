4416b8742ebdd2005c36583937305fe5c10fb296
SHAIKH DANISH <91310497+Danishds0@users.noreply.github.com>
2023-06-05 21:33:13 +0530

    Update **Formula.java

--- **Formula.java
+++ **Formula.java
@@ -4,3 +4,10 @@ int[][] resultArray = list.stream().map(innerList -> innerList.stream().mapToInt
 
 Convert 2D Array to 2D List
 List<List<Integer>> list = Arrays.stream(resultArray).map(row -> Arrays.asList(row)).collect(Collectors.toList());
+
+To Soert 2D array in a increasing form
+Arrays.sort(resultArray, (a, b) -> a[0] - b[0]);
+
+To Soert 2D array in a increasing form
+Arrays.sort(resultArray, (a, b) -> b[0] - a[0]);
+
