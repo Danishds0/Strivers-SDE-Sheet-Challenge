7e241f24977a20c4e434f60b819e6d8bd193c20f
SHAIKH DANISH <91310497+Danishds0@users.noreply.github.com>
2023-06-06 13:03:08 +0530

    Update **Formula.java

--- **Formula.java
+++ **Formula.java
@@ -11,3 +11,11 @@ Arrays.sort(resultArray, (a, b) -> a[0] - b[0]);
 To Soert 2D array in a increasing form
 Arrays.sort(resultArray, (a, b) -> b[0] - a[0]);
 
+Sum of all element in arrayList
+int sum = arr.stream().reduce(0, Integer::sum);
+
+Sum of all element in Array
+int sum = arr.stream().sum();
+
+Max in the integer in Array
+int max = arr.stream().max().getAsInt();
