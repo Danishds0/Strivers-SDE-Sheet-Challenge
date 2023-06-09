f0a4c706b2d8f0dde63ecb79f59812eaac1c95b2
SHAIKH DANISH <91310497+Danishds0@users.noreply.github.com>
2023-06-09 18:09:18 +0530

    Create 6.9 Detect-Cycle-in-Linked-List.java

---
+++ 6.9 Detect-Cycle-in-Linked-List.java
@@ -0,0 +1,23 @@
+// Given head, the head of a linked list, determine if the linked list has a cycle in it.
+// There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. 
+// Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+// Return true if there is a cycle in the linked list. Otherwise, return false.
+
+// https://leetcode.com/problems/linked-list-cycle/
+
+// Brute Force Approach 
+// Using set, the Hashset stores the node and when the repeated node already contains in set then it will return true else false
+
+// Optimal Solution slow-fast pointer
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head, fast = head, entry = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if(slow == fast) return true;
+        }
+        return false;
+    }
+}
