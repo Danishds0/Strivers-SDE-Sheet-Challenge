f186cd3460cbd47e929c612d93336ad4f7eabe94
SHAIKH DANISH <91310497+Danishds0@users.noreply.github.com>
2023-06-15 19:39:43 +0530

    Update 11.2 Median-in-a-row-wise-sorted-Matrix.java

--- 11.2 Median-in-a-row-wise-sorted-Matrix.java
+++ 11.2 Median-in-a-row-wise-sorted-Matrix.java
@@ -48,3 +48,31 @@ class Solution {
         return start;
     }
 }
+
+
+
+//Another way to solve
+
+int countSmallerThenEqualToMid(int[] matrix, int mid){
+        int start = 0, end = matrix.length - 1;
+        while(start <= end){
+            int middle = start + (end - start) / 2;
+            if(matrix[middle] <= mid) start = middle + 1;
+            else end = middle - 1;
+        }
+        return start;
+    }
+    int median(int matrix[][], int R, int C) {
+        // code here   
+        int start = 1, end = 2000;
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            int count = 0;
+            for(int i = 0; i < R; i++){
+                count += countSmallerThenEqualToMid(matrix[i], mid);
+            }
+            if(count <= (R * C) / 2) start = mid + 1;
+            else end = mid - 1;
+        }
+        return start;
+    }
