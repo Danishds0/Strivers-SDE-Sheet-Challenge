7abe44b2caaf1cee431bed8c75ed22334c66a7bb
SHAIKH DANISH <91310497+Danishds0@users.noreply.github.com>
2023-06-22 16:36:18 +0530

    Update 17.3 Valid-Parentheses.java

--- 17.3 Valid-Parentheses.java
+++ 17.3 Valid-Parentheses.java
@@ -27,3 +27,27 @@ class Solution {
         return st.empty();
     }
 }
+
+//One more approach using stringBuilder 
+
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length() % 2 != 0) return false;
+        int top = -1;
+        StringBuilder sb = new StringBuilder(s);
+        for(int i = 0; i < s.length(); i++){
+            if(top < 0 || !isMatch(sb.charAt(top), sb.charAt(i))){
+                top++;
+                sb.setCharAt(top, sb.charAt(i));
+            }
+            else top--;
+        }
+        return top == -1;
+    }
+    private boolean isMatch(char a, char b){
+        if(a == '(' && b == ')') return true;
+        if(a == '{' && b == '}') return true;
+        if(a == '[' && b == ']') return true;
+        return false;
+    }
+}
