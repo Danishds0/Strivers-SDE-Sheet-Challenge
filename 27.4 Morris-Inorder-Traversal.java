59f326fe1762f44a0388bb3925b3e50c02dbce31
SHAIKH DANISH <91310497+Danishds0@users.noreply.github.com>
2023-07-09 18:22:41 +0530

    Create 27.4 Morris-Inorder-Traversal.java

---
+++ 27.4 Morris-Inorder-Traversal.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        TreeNode curr = root;
+        TreeNode prev;
+
+        while(curr != null){
+            if(curr.left == null){
+                ans.add(curr.val);
+                curr = curr.right;
+            }
+            else{
+                prev = curr.left;
+
+                while(prev.right != null){
+                    prev = prev.right;
+                }
+
+                prev.right = curr;
+                TreeNode temp = curr;
+                curr = curr.left;
+
+                temp.left = null;
+            }
+        }
+        return ans;
+    }
+}
