b4da80a5cb506d0fe3cf63a99dadf62e0a9a368d
SHAIKH DANISH <91310497+Danishds0@users.noreply.github.com>
2023-07-09 18:18:29 +0530

    Create 27.1 Inorder-Traversal.java

---
+++ 27.1 Inorder-Traversal.java
@@ -0,0 +1,22 @@
+// Given a Binary Tree, return Left view of it. Left view of a Binary Tree is set of nodes visible when tree is visited from Left side. The task is to complete the function leftView(), which accepts root of the tree as argument.
+
+// Left view of following tree is 1 2 4 8.
+
+//           1
+//        /     \
+//      2        3
+//    /     \    /    \
+//   4     5   6    7
+//    \
+//      8   
+
+class Solution {
+    ArrayList<Integer> list = new ArrayList<Integer>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+     if(root == null) return list;
+        inorderTraversal(root.left);
+        list.add(root.val);
+        inorderTraversal(root.right);
+        return list;
+    }
+}
